0476cda19e3757f92f26ab876ed250b789aa8f4d
Soumya <101737464+soumyavatti@users.noreply.github.com>
2025-04-18 08:34:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,55 @@
 const movies = [
-  {
-    title: "Inception",
-    image: "https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg",
-  },
-  {
-    title: "Interstellar",
-    image: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg",
-  },
-  {
-    title: "Tenet",
-    image: "https://image.tmdb.org/t/p/w500/k68nPLbIST6NP96JmTxmZijEvCA.jpg",
-  },
-  {
-    title: "The Dark Knight",
-    image: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
-  },
-  {
-    title: "Dunkirk",
-    image: "https://image.tmdb.org/t/p/w500/ebSnODDg9lbsMIaWg2uAbjn7TO5.jpg",
-  },
+  { title: "Inception", image: "https://image.tmdb.org/t/p/w500/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg" },
+  { title: "Interstellar", image: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg" },
+  { title: "Tenet", image: "https://image.tmdb.org/t/p/w500/k68nPLbIST6NP96JmTxmZijEvCA.jpg" },
+  { title: "The Dark Knight", image: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg" },
 ];
 
 const movieRow = document.getElementById("movieRow");
+const myListRow = document.getElementById("myListRow");
 
-movies.forEach((movie) => {
-  const img = document.createElement("img");
-  img.src = movie.image;
-  img.alt = movie.title;
-  movieRow.appendChild(img);
-});
+function renderMovies(container, movieArray) {
+  container.innerHTML = "";
+  movieArray.forEach((movie) => {
+    const img = document.createElement("img");
+    img.src = movie.image;
+    img.alt = movie.title;
+    img.title = "Click to add/remove from My List";
+    img.onclick = () => toggleMyList(movie);
+    container.appendChild(img);
+  });
+}
+
+function getMyList() {
+  const list = localStorage.getItem("myList");
+  return list ? JSON.parse(list) : [];
+}
+
+function saveMyList(list) {
+  localStorage.setItem("myList", JSON.stringify(list));
+}
+
+function toggleMyList(movie) {
+  let myList = getMyList();
+  const exists = myList.find((m) => m.title === movie.title);
+  if (exists) {
+    myList = myList.filter((m) => m.title !== movie.title);
+  } else {
+    myList.push(movie);
+  }
+  saveMyList(myList);
+  renderMovies(myListRow, myList);
+}
+
+function addToList(title) {
+  const movie = movies.find((m) => m.title === title);
+  if (movie) toggleMyList(movie);
+}
+
+function playMovie() {
+  alert("Playing movie (placeholder)");
+}
+
+// Initial render
+renderMovies(movieRow, movies);
+renderMovies(myListRow, getMyList());
